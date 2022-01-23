e82e6ca6a5e9f85e0e47f3a46689700745b6d8f9
stdg3 <velikoglosv@mail.ru>
2022-01-24 01:26:20 +0200

    clean code - comments

--- iawSwCaseDeneme.ino
+++ iawSwCaseDeneme.ino
@@ -1,7 +1,7 @@
 int state = 1;
 volatile uint32_t lastRead = 0;
-volatile uint32_t debounce; //kolebaniya
-int interval = 5;
+volatile uint32_t debounce;
+int interval = 10;
 int yellowInterval = 3;
 
 bool doPause = false;
@@ -12,11 +12,12 @@ int yellowLed = 5;
 int greenLed = 6;
 
 void setup() {
-  //interrupts
+
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   attachInterrupt(0, GreenInterrupt, CHANGE);
   attachInterrupt(1, RedInterrupt, CHANGE);
+
   pinMode(redLed, OUTPUT);
   pinMode(yellowLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
@@ -24,12 +25,8 @@ void setup() {
 }
 
 void GreenInterrupt() {
-  // оставим 100 мс таймаут на гашение дребезга
-  // CHANGE не предоставляет состояние пина, 
-  // придётся узнать его при помощи digitalRead
   if (millis() - debounce >= 100 && digitalRead(2)) {
     debounce = millis();
-    // ваш код по прерыванию по высокому сигналу
     if (state == 1){
         state=2;
       }
@@ -37,19 +34,14 @@ void GreenInterrupt() {
       state = 4;
       lastRead = millis()/1000;
       doPause = true; 
-      }
-    
+      }    
   }
 }
 
 
 void RedInterrupt() {
-  // оставим 100 мс таймаут на гашение дребезга
-  // CHANGE не предоставляет состояние пина, 
-  // придётся узнать его при помощи digitalRead
-  if (millis() - debounce >= 100 && digitalRead(3)) {
+    if (millis() - debounce >= 100 && digitalRead(3)) {
     debounce = millis();
-    // ваш код по прерыванию по высокому сигналу
     switch(state){
       case 2:
         state=3;
@@ -68,27 +60,12 @@ void RedInterrupt() {
         state=3;
         lastRead = millis()/1000;
         break;
-      }
-    /*if (state == 3){        
-        doPause = true;
-        lastRead = millis()/1000;
-        state=1;
-      }
-    else if (state == 4){        
-        from5to3 = true;
-        state=5;
-      }
-    else if (state == 5){
-        state=3;
-        lastRead = millis()/1000;
-      }*/
-    
+      }    
   }
 }
 
 void RedON(){
   if (millis()/1000 - lastRead <= interval){
-    //lastRead += 1;
     digitalWrite(redLed, HIGH);
     digitalWrite(yellowLed, LOW);
     digitalWrite(greenLed, LOW);    
@@ -101,7 +78,6 @@ void RedON(){
   }
 
 void YellowON(){
-  //lastRead = millis()/1000;
     if (millis()/1000 - lastRead <= yellowInterval){
       digitalWrite(redLed, LOW);
       digitalWrite(yellowLed, HIGH);
@@ -112,7 +88,6 @@ void YellowON(){
       lastRead = millis()/1000;   
     }
   }
-
 
 void GreenON(){
   if (millis()/1000 - lastRead <= interval){
@@ -125,31 +100,9 @@ void GreenON(){
     state = 5;    
     }
   }
-  int cc = 0;
-bool rls = false;
 
 void RedBlink(){
-  digitalWrite(redLed, LOW);
-      /*if (millis() - lastRead >= 1000) {   
-        lastRead += 1000;
-        switch (rls){
-          case true:
-            digitalWrite(redLed, LOW);
-            rls = false;
-            break;
-          case false:
-            digitalWrite(redLed, HIGH);
-            rls = true;
-            cc =cc + 1;
-            if (cc == 3){
-            cc = 0;
-            state = 3;
-            lastRead = millis()/1000;
-             }
-            break;
-          }       
-        }*/
-        
+  digitalWrite(redLed, LOW);      
     for (int i = 0; i <3; i++){
     digitalWrite(redLed, LOW);
     delay(1000);
@@ -178,8 +131,7 @@ void GreenBlink(){
     lastRead = millis()/1000;
   }
 
-void loop() {
-  
+void loop() {  
   switch(state){
     case 1:
     if (doPause){
